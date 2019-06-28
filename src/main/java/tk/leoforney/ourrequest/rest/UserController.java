67d10a949ac07cfffe6dddaa7a0f45eb01b81e81
Leo <forneyleo@gmail.com>
2019-06-28 09:27:39 -0500

    Remove pki validation files add banner, and add redirection endpoint

--- src/main/java/tk/leoforney/ourrequest/rest/UserController.java
+++ src/main/java/tk/leoforney/ourrequest/rest/UserController.java
@@ -16,9 +16,41 @@ public class UserController {
     }
 
     @RequestMapping(value = "/OurRequest-1/{wildcard}", method = RequestMethod.GET)
-    String handleFoo(@PathVariable("wildcard") String route, HttpServletRequest request) throws IOException {
-        System.out.println(route);
-        return request.getContextPath();
+    void handleRedirect(@PathVariable("wildcard") String route, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //System.out.println(request.getRequestURI());
+        //String[] split = request.getRequestURL().toString().split("/OurRequest-1/");
+        //System.out.println(request.getScheme() + "://" + request.getServerName() + ":" + request.);
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        response.setHeader("Location", getURL(request));
+    }
+
+    public static String getURL(HttpServletRequest req) {
+
+        String scheme = req.getScheme();             // http
+        String serverName = req.getServerName();     // hostname.com
+        int serverPort = req.getServerPort();        // 80
+        //String contextPath = req.getContextPath();   // /mywebapp
+        String servletPath = req.getServletPath().replace("/OurRequest-1", "");   // /servlet/MyServlet
+        String pathInfo = req.getPathInfo();         // /a/b;c=123
+        String queryString = req.getQueryString();          // d=789
+
+        // Reconstruct original requesting URL
+        StringBuilder url = new StringBuilder();
+        url.append(scheme).append("://").append(serverName);
+
+        if (serverPort != 80 && serverPort != 443) {
+            url.append(":").append(serverPort);
+        }
+
+        url.append(servletPath);
+
+        if (pathInfo != null) {
+            url.append(pathInfo);
+        }
+        if (queryString != null) {
+            url.append("?").append(queryString);
+        }
+        return url.toString();
     }
 
 }
